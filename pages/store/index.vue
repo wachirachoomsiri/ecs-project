73a8edf19b1455e193ace270f6f3ef75939beba2
KoonPaT <sutthipat.litthibanlue@gmail.com>
2023-12-11 01:19:18 +0700

    Update index.vue

--- pages/store/index.vue
+++ pages/store/index.vue
@@ -1,8 +1,34 @@
+
 <template>
-    <section class="flex items-center justify-center h-screen">
+    <section>
         <div>
-            <b-button @click="$router.push('/store/ticket')" type="is-dark">จองโต๊ะ</b-button>
-            <b-button @click="$router.push('/store/shirt')" type="is-dark">จองเสื้อ</b-button>
+            <div class="flex flex-col items-center mx-auto mb-8">
+                <div class="grid grid-cols-3 gap-4 w-96 mt-4 mb-0 ">
+                    <b-button @click="$router.push('/store/ticket')" type="is-dark">จองโต๊ะ</b-button>
+                    <b-button @click="$router.push('/store/shirt')" type="is-dark">จองเสื้อ</b-button>
+                    <b-button @click="$router.push('/store/shirt')" type="is-dark">จองเสื้อ</b-button>
+                </div>
+                <div class="text-center">
+                    <br>
+                    <div>
+                        <div class="flex flex-col items-center justify-center mx-4 h-auto ">
+                            <div class="box-border bg-white w-auto p-16 rounded-xl shadow-orange-300 shadow-2xl py-4">
+                                <div>
+                                    <img src="/gear.gif" alt="image" class="mx-auto mb-auto w-48 h-48">
+                                    <h1 class="text-4xl text-gray-500 font-silpakorn mb-4">ยินดีต้อนรับเข้าสู่ EE XVII
+                                        HOMECOMING</h1>
+                                    <h1 class="text-4xl text-gray-500 font-silpakorn mb-4">งานจัดขึ้นในวันที่ 24
+                                        กุมภาพันธ์
+                                        2566</h1>
+                                    <h1 class="text-4xl text-gray-500 font-silpakorn mb-4">ณ
+                                        ศูนย์ศิลปวัฒนธรรมเฉลิมพระเกียรติ</h1>
+                                    <h1 class="text-4xl text-gray-500 font-silpakorn mb-4">6 รอบพระชนมพรรษา</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
